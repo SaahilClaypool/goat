db2a889ebd941080c6e066ea4d40ea41d8a992b9
saahil <saahil@localhost.localdomain>
2016-10-18 11:15:02 -0400

    init

---
+++ app.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+	"log"
+	"time"
+	"io"
+	"bytes"
+)
+var taskMap map[string]Task 
+// read the init file for all of the tasks
+func Init(){
+	taskMap = make(map[string]Task)// create task map
+	// open and read file
+	fileName := "./temp.txt"
+	input , err := os.Open(fileName)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	dec := json.NewDecoder(input)
+
+	for{
+		var a_task Task; 
+		err = dec.Decode(&a_task)
+		if(err != nil){
+			if(err == io.EOF ){
+				break; 
+			}
+			log.Fatal(err)
+		}
+		// add task to map
+		taskMap[a_task.Name] = a_task
+	}
+}
+
+
+
+func CreateTask (name string) string{
+	task , exist := taskMap[name]
+	if(exist){
+		if task.Active{
+			return fmt.Sprintf("task %s is already active" , name)
+		} else{
+			// append a new time value
+			newInt := TimeInterval{
+				time.Now(), 
+				time.Now(),
+			}
+			task.Active = true
+			task.TimeIntervals = append([]TimeInterval{newInt}, task.TimeIntervals ...)
+			taskMap[name] = task 
+			
+			return fmt.Sprintf("restarted task: %s", name)
+
+		}
+	}
+	new_task := Task{
+		true, 
+		name,
+		make([]TimeInterval, 0),
+		make([]string, 0), 
+	}
+	now := time.Now()
+	new_task.TimeIntervals = append(new_task.TimeIntervals, TimeInterval{now, now})
+
+	taskMap[name] = new_task
+	return fmt.Sprintf("created new task: %s", name)
+
+}
+func EndTask(name string){
+	now := time.Now()
+
+	task , exist := taskMap[name]
+	if(exist){
+		task.Active = false
+		task.TimeIntervals[0].End = now 
+		taskMap[name] = task
+		fmt.Printf("removed : %s\n" , name)
+	}
+}
+func ListTasks() []string{
+	keys := make([]string, 0)
+	for k,_ := range(taskMap){
+		 keys = append(keys, k)
+	}
+	return keys
+}
+func AddNote(taskname string, note string){
+	task := taskMap[taskname]
+	task.Notes = append(task.Notes, note)
+	taskMap[taskname] = task
+}
+
+func Info(taskname string) string{
+	task := taskMap[taskname]
+	return fmt.Sprintf("%s\n", task.String())
+}
+func WriteTasks (){
+	fileName := "./temp.txt"
+	output, err := os.Create(fileName)
+	if err != nil{
+		log.Fatal(err); 
+	}
+
+	for _, v := range (taskMap){
+		fmt.Fprintf(output, "%s\n", v.ToJson()); 
+	}
+}
+
+func CurrentTasks() []Task {
+	currentTasks := make([]Task, 0)
+	for _,v := range(taskMap){
+		if(v.Active){
+			currentTasks = append(currentTasks, v)
+		}
+	}
+	return currentTasks
+}
+
+type TimeInterval struct {
+	Start time.Time ;
+	End time.Time ; 
+}
+func (timeInterval *TimeInterval) duration() time.Duration {
+	return timeInterval.End.Sub( timeInterval.Start)
+}
+
+type Task struct {
+	Active bool 
+	Name string
+	TimeIntervals []TimeInterval
+	// Start_time time / int
+	Notes []string; 
+}
+
+func (task *Task) String() string{
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("Name: %s\nActive: %t\nDuration: %d\nNotes:\n", task.Name, task.Active, task.TimeIntervals[0].duration()))
+	for _, nt := range(task.Notes){
+		buffer.WriteString(fmt.Sprintf("\t%s\n", nt))
+	}
+	return buffer.String(); 
+	
+}
+func (task *Task) Write(file *os.File) error{
+	fmt.Fprintf(file, "%s\n", task.ToJson())
+	return nil
+}
+func (task *Task) ToJson () []byte {
+	jsonStr , err := json.Marshal(task)
+	if err != nil{
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	
+	return jsonStr
+}
+
---
+++ goat.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+)
+func main(){
+	// temp
+	// textObj := TextObject{"text object!", 9}
+	fmt.Printf("main of goat!\n")
+	app := cli.NewApp()
+	app.Name = "goat"
+	app.Usage = "time all your daily tasks!"
+	app.Commands = []cli.Command{
+		{
+			Name: "start", 
+			Aliases:  []string{"st" , "s"}, 
+			Usage: "create an entry for the given task name", 
+			Action: func(c *cli.Context) error{
+				fmt.Printf("started task : %s\n", c.Args().First())
+				Init()
+				CreateTask(c.Args().First())
+				WriteTasks(); 
+				return nil 
+
+			}, 
+			
+		}, 
+		{
+			Name: "current", 
+			Usage: "print current tasks", 
+			Aliases: []string{"cur"}, 
+			Action: func (c *cli.Context) error{
+				Init()
+				fmt.Printf("%v\n", CurrentTasks())
+				return nil
+			}, 
+		}, 
+		{
+			Name: "end",
+			Usage: "end a given task", 
+			Action: func (c *cli.Context) error{
+				Init()
+				EndTask(c.Args().First())
+				WriteTasks(); 
+				return nil 
+			}, 
+		}, 
+		{
+			Name: "list", 
+			Usage: "list tasks", 
+			Aliases: []string{"ls"}, 
+			Action: func (c *cli.Context) error{
+				Init()
+				fmt.Printf("%v\n", ListTasks())
+				return nil
+			}, 
+		}, 
+		{
+			Name: "note", 
+			Usage:"create a note for a specific task\n\tgoat note [task name] \"a note\"",
+			Aliases: []string{"nt"},
+			Action: func (c *cli.Context) error{
+				Init()
+				fmt.Printf("adding note...\n")
+				AddNote(c.Args().First(), c.Args()[1])
+				WriteTasks(); 
+				return nil 
+			}, 
+		},
+		{
+			Name:"info", 
+			Usage: "detailed info on a task", 
+			Aliases: []string{"i"}, 
+			Action: func (c *cli.Context) error{
+				Init()
+				fmt.Printf("%s\n", Info(c.Args()[0]))
+				return nil 
+			}, 
+		}, 
+		
+	}
+	// app.Flags = []cli.Flag{
+	// 	cli.StringFlag{
+	// 		// name task name
+	// 		Name: "name"
+	// 		// usage
+	// 		Usage: "name of the task to be executed"
+
+	// 		// value
+	// 		// destination
+	// 		Destination: &the_name; 
+			
+	// 	}
+	// }
+	// app.Action = func(c *cli.Context) error {
+	// 	fmt.Printf("Hello %q\n", c.Args())
+	// 	return nil 
+	// }
+	app.Run(os.Args)
+
+}
+
+
